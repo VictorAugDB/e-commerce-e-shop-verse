077530f15e45c9477c92e99aa4c3a221616721ee
VictorAugDB <vdbernardes@gmail.com>
2023-06-22 21:01:41 -0300

    feat: create header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,22 +3,27 @@ import * as React from 'react';
 import UnstyledLink from '@/components/links/UnstyledLink';
 
 const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+  { href: '/', label: 'Home' },
+  { href: '/contact', label: 'Contact' },
+  { href: '/about', label: 'About' },
+  { href: '/sign-up', label: 'Sign Up' },
 ];
 
 export default function Header() {
   return (
     <header className='sticky top-0 z-50 bg-white'>
-      <div className='layout flex h-14 items-center justify-between'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
-        </UnstyledLink>
+      <div className='layout flex h-14 items-center justify-around'>
+        <h3 className='font-bold'>Exclusive</h3>
         <nav>
-          <ul className='flex items-center justify-between space-x-4'>
+          <ul className='flex items-center justify-between gap-x-12'>
             {links.map(({ href, label }) => (
               <li key={`${href}${label}`}>
-                <UnstyledLink href={href} className='hover:text-gray-600'>
+                <UnstyledLink
+                  href={href}
+                  className={`text-base hover:text-gray-600 ${
+                    label === 'Home' && 'border-b border-gray-500'
+                  }`}
+                >
                   {label}
                 </UnstyledLink>
               </li>
