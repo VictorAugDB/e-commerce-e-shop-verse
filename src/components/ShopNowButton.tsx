03d257bd218d9bb15fc898a01eed28fd688229ea
VictorAugDB <vdbernardes@gmail.com>
2023-07-08 18:44:11 -0300

    refactor: separe ShowNowButton into a component

---
+++ src/components/ShopNowButton.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import React from 'react';
+
+type ShopNowButtonProps = {
+  children?: React.ReactNode;
+  href: string;
+};
+
+export default function ShopNowButton({ children, href }: ShopNowButtonProps) {
+  return (
+    <Link className='flex items-center gap-2' href={href}>
+      <p className='border-spacing-4 border-b border-white pb-[0.15rem] text-white'>
+        Shop now
+      </p>
+      {children}
+    </Link>
+  );
+}
