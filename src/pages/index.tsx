f6f0df4be01e9246aa8bb4daaaf7d3bbceff2889
VictorAugDB <vdbernardes@gmail.com>
2023-07-11 20:35:24 -0300

    feat: render correct RoundedBackgroundIcons in the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,8 @@ import { BsPhone, BsSmartwatch } from 'react-icons/bs';
 import { FiHeadphones } from 'react-icons/fi';
 import { HiOutlineDesktopComputer } from 'react-icons/hi';
 import { LuGamepad } from 'react-icons/lu';
-import { TbTruckDelivery } from 'react-icons/tb';
+import { LuShieldCheck } from 'react-icons/lu';
+import { TbHeadphones, TbTruckDelivery } from 'react-icons/tb';
 
 import BackgroundProduct from '@/components/BackgroundProduct';
 import Button from '@/components/buttons/Button';
@@ -229,8 +230,28 @@ export default function HomePage() {
           />
         </div>
       </div>
-      <div className='flex items-center'>
-        <RoundedBackgroundIcon icon={TbTruckDelivery} />
+      <div className='flex items-center gap-[5.5rem]'>
+        <div className='flex flex-col items-center gap-6'>
+          <RoundedBackgroundIcon icon={TbTruckDelivery} />
+          <div className='flex flex-col items-center gap-2'>
+            <p className='text-lg font-semibold'>FREE AND FAST DELIVERY</p>
+            <p className='text-sm'>Free delivery for all orders over $140</p>
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-6'>
+          <RoundedBackgroundIcon icon={TbHeadphones} />
+          <div className='flex flex-col items-center gap-2'>
+            <p className='text-lg font-semibold'>24/7 CUSTOMER SERVICE</p>
+            <p className='text-sm'>Friendly 24/7 customer supporth</p>
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-6'>
+          <RoundedBackgroundIcon icon={LuShieldCheck} />
+          <div className='flex flex-col items-center gap-2'>
+            <p className='text-lg font-semibold'>MONEY BACK GUARANTEE</p>
+            <p className='text-sm'>We reurn money within 30 days</p>
+          </div>
+        </div>
       </div>
     </div>
   );
