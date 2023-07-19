cbc216c0810e17c51d3227a03f26e82e754979d0
VictorAugDB <vdbernardes@gmail.com>
2023-07-19 19:32:48 -0300

    feat: finish sign up page

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,11 +1,18 @@
 import Button from '@/components/buttons/Button';
+import NextImage from '@/components/NextImage';
 
 export default function signUp() {
   return (
-    <div className='flex flex-1 gap-32'>
-      <div></div>
-      <div className='flex flex-col gap-12'>
-        <div className='flex flex-col gap-6'>
+    <div className='grid grid-cols-2 gap-32 pt-[3.75rem]'>
+      <div className='relative'>
+        <NextImage
+          alt='product-image'
+          src='/images/cart-cellphone.png'
+          fill
+        ></NextImage>
+      </div>
+      <div className='flex w-max flex-col gap-12'>
+        <div className='flex flex-col gap-6 pr-[3.4375rem]'>
           <h1>Create an account</h1>
           <p>Enter your details below</p>
         </div>
@@ -19,15 +26,19 @@ export default function signUp() {
             <Button variant='green' className='flex w-full justify-center py-4'>
               Create Account
             </Button>
-            <Button variant='green' className='flex w-full justify-center py-4'>
-              Google Button Here
+            <Button
+              variant='ghost'
+              className='flex w-full justify-center gap-4 rounded border border-gray-600 py-4'
+            >
+              <img src='/images/google-icon.png' alt='' />
+              Sign up with Google
             </Button>
           </div>
           <div className='flex justify-center gap-4'>
             <p>Already have account?</p>
             <a
               href='/sign-in'
-              className='border-b border-gray-800 pb-1 font-medium text-gray-800'
+              className='border-b border-gray-800 pb-1 font-medium text-gray-800 hover:font-bold'
             >
               Log in
             </a>
