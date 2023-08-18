402dac0bb01318e7005ebf3a6bc984916af75896
VictorAugDB <vdbernardes@gmail.com>
2023-08-18 15:21:24 -0300

    refactor: update next api url to get the VERCEL_URL if it exists and localhost if not

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -33,19 +33,28 @@ export const getProducts = async ({
     query += `${query ? '&' : '?'}_sort=numberOfSales&_order=desc`
   }
 
-  const res = await fetch('http://localhost:3000/api/products' + query, {
-    next: {
-      revalidate: revalidate ? 60 * 60 * revalidate : 0,
-    }, // X hours
-  })
+  const res = await fetch(
+    (process.env.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000/') +
+      'api/products' +
+      query,
+    {
+      next: {
+        revalidate: revalidate ? 60 * 60 * revalidate : 0,
+      }, // X hours
+    },
+  )
 
   return await res.json()
 }
 
 export const getProductById = async (id: string): Promise<Product> => {
-  const res = await fetch(`http://localhost:3000/api/products/${id}`, {
-    next: { revalidate: 60 * 60 * 12 },
-  }) // 12 hours
+  const res = await fetch(
+    (process.env.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000/') +
+      `api/products/${id}`,
+    {
+      next: { revalidate: 60 * 60 * 12 },
+    },
+  ) // 12 hours
 
   return res.status === 204 ? null : res.json()
 }
@@ -56,11 +65,17 @@ export const getProductsByIds = async (ids: string[]): Promise<Product[]> => {
   }
 
   const queryParams = ids.map((id) => `id=${id}`).join('&')
-  const res = await fetch(`http://localhost:3000/api/products?${queryParams}`)
+  const res = await fetch(
+    (process.env.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000/') +
+      `api/products?${queryParams}`,
+  )
   return res.json()
 }
 
 export const getCoupons = async (): Promise<Coupon[]> => {
-  const res = await fetch('http://localhost:3000/api/coupons')
+  const res = await fetch(
+    (process.env.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000/') +
+      'api/coupons',
+  )
   return await res.json()
 }
