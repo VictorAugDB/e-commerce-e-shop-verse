5a306b5964708fc6c042c9b4a95260c0dc1a97ca
VictorAugDB <vdbernardes@gmail.com>
2023-08-23 19:34:46 -0300

    feat: add api/auth endpoint with google probider and mongo adapter

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,23 @@
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import NextAuth, { AuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+import clientPromise from '@/lib/mongo'
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise, {
+    databaseName: 'e-shopverse',
+  }),
+}
+
+const handler = NextAuth({
+  ...authOptions,
+})
+
+export { handler as GET, handler as POST }
